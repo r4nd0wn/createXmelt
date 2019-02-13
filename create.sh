a7b09e06ddc58fdff3abdbd0d0d6b2b877618e5f
Joans <jonas@jbach.xyz>
2019-02-13 16:21:56 +0100

    Update create.sh

--- create.sh
+++ create.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-echo "please make shure your domain is in that scheme: subsub[raisingNumber.subdomain.sag.tools"
+echo "please make shure your domain is in that scheme: subsub[raisingNumber].subdomain.sag.tools"
 
-read -r -p "Type in the absolute path to the installed directory" path
+read -r -p "Type in the absolute path to the installed directory " path
 
-read -r -p "Whats the name of your first sag.tools subdomain?" sub
-read -r -p "Whats the name of your subsub domain?" subsub
-read -r -p "Whats the name of your E-Mail Prefix" Prefix
-read -r -p "Type in your password" password
-read -r -p "Type in your api key" apikey
-read -r -p "Type in the totalAccount to Create" totalAccount
-read -r -p "how many instances do you want to create?" amount
+read -r -p "Whats the name of your first sag.tools subdomain? " sub
+read -r -p "Whats the name of your subsub domain? " subsub
+read -r -p "Whats the name of your E-Mail Prefix " Prefix
+read -r -p "Type in your password " password
+read -r -p "Type in your api key " apikey
+read -r -p "Type in the totalAccount to Create " totalAccount
+read -r -p "how many instances do you want to create? " amount
 cd $path
 mkdir ../instances
 
